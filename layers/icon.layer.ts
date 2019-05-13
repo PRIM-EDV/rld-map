58acc1ae0bb818e8313d3800d173e7e6de2ce0d1
Lucas <lucasmoesch@live.de>
2019-05-13 08:57:22 +0200

    Work in progress

---
+++ layers/icon.layer.ts
@@ -0,0 +1,10 @@
+import { Layer } from './layer';
+import { MapObject } from '../backend/backend.service';
+
+export class IconLayer extends Layer {
+    private _mapObjects: Array<MapObject> = [];
+
+    public draw() {
+
+    }
+}
